c02962d4734b017babc15609a55ee456a2e545a8
lowol <@lowol>
2012-07-17 12:41:33 +0900

    v0.0.2

--- formatdate.js
+++ formatdate.js
@@ -9,12 +9,22 @@ var moment = require('moment');
  */
 function FormatDate(path, options) {
   SchemaTypes.Date.call(this, path, options);
+
+  this.validators.push([function(v) {
+    return v instanceof Date;
+  }, 'format']);
 }
 FormatDate.prototype = Object.create(SchemaTypes.Date.prototype);
 FormatDate.prototype.cast = function(value) {
+  if (value === null || value === '') {
+    return null;
+  }
+
   if (value instanceof Date) {
     return value;
-  } else if (value instanceof Number || 
+  }
+
+  if (value instanceof Number ||
       'number' == typeof value || 
       String(value) == Number(value)) {
     // support for timestamps
@@ -22,13 +32,16 @@ FormatDate.prototype.cast = function(value) {
   }
 
   // format date string
-  var format = this.options.format || 'YYYY-MM-DD hh:mm:ss';
+  var format = this.options.format || 'YYYY-MM-DD';
   var mdate = moment(value.toString() || '', format);
+
   if (mdate.format(format) != value) {
-    throw new CastError('date', value);
+    // @todo hmmm....
+    //throw new CastError('formatdate', value);
+    return value;
   }
 
   return mdate.toDate();
 };
 
-mongoose.Schema.Types.FormatDate = FormatDate;
+module.exports = mongoose.Schema.Types.FormatDate = FormatDate;
